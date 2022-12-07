7a751dce0ab72d4b10e39b145cbf94724d7c0af8
Jordan Rhode <2262659+JordanRhode@users.noreply.github.com>
2022-12-07 13:05:15 -0600

    Camp Cleanup

--- AdventOfCode.Solutions/Year2022/Day04/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day04/Solution.cs
@@ -2,15 +2,29 @@ namespace AdventOfCode.Solutions.Year2022.Day04;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(04, 2022, "") { }
+    public Solution() : base(04, 2022, "Camp Cleanup", false) { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var input = Input.SplitByNewline().Select(i => i.Split(',').Select(i => i.Split('-').Select(int.Parse).ToArray()).ToArray());
+        return input.Count(i => {
+            var leftMin = i[0][0];
+            var rightMin = i[1][0];
+            var leftMax = i[0][1];
+            var rightMax = i[1][1];
+            return (leftMin >= rightMin && leftMax <= rightMax) || (leftMin <= rightMin && leftMax >= rightMax);
+        } ).ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var input = Input.SplitByNewline().Select(i => i.Split(',').Select(i => i.Split('-').Select(int.Parse).ToArray()).ToArray());
+        return input.Count(i => {
+            var leftMin = i[0][0];
+            var rightMin = i[1][0];
+            var leftMax = i[0][1];
+            var rightMax = i[1][1];
+            return (leftMin >= rightMin && leftMin <= rightMax) || (leftMax >= rightMin && leftMax <= rightMax) || (rightMin >= leftMin && rightMin <= leftMax) || (rightMax >= leftMin && rightMax <= leftMax);
+        }).ToString();
     }
 }
